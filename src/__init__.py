1ae729056394893bb128bb0900acf8fe6cbfe76e
Sylvain Loiseau <sylvain.loiseau@univ-paris13.fr>
2022-04-26 10:23:01 +0200

    import

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
-# from .igt import Corpus, Text, Paragraph, Sentence, Word, Morph
-# from .elan import ElanCorpoAfr
-# from .emeld import Emeld
+from .igt import Corpus, Text, Paragraph, Sentence, Word, Morph
+from .elan import ElanCorpoAfr
+from .emeld import Emeld 
+from .json import ToJson
