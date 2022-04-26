603dd104b54f69c866719427c3677cbf0f5ee6ca
Sylvain Loiseau <sylvain.loiseau@univ-paris13.fr>
2022-04-26 09:26:49 +0200

    debugged

--- src/json.py
+++ src/json.py
@@ -1,25 +1,23 @@
 import json
 from igttools import Corpus, LingUnit, NonTerminalLingUnit, Properties
-from Emeld import ORDERED_LEVEL
+from igttools.emeld import ORDERED_LEVEL
 
 class ToJson():
 
-    @classmethod
-    def to_json(corpus:Corpus, outfile: str):
-        res = walk_corpus(corpus, 0)
-        with open(outfile, "wb") as f:
-          json.dump(res, f, indent=4)
-
     def walk_corpus(unit: LingUnit, level:int):
         res = {}
-        if unit.properties: Properties is not None:
+        if unit.properties is not None:
             res["item"] = {}
-            for k,v in unit.properties.items()
+            for k,v in unit.properties.items():
               res["item"][k] = v
         if isinstance(unit, NonTerminalLingUnit) and unit.items is not None:
             sub_level = ORDERED_LEVEL[level][1]
-            res[sub_level] = [walk_corpus(u, level+1) for u in unit.items]
+            res[sub_level] = [ToJson.walk_corpus(u, level+1) for u in unit.items]
         return res
-              
 
+    @classmethod
+    def to_json(corpus:Corpus, outfile: str):
+        res = ToJson.walk_corpus(corpus, 0)
+        with open(outfile, "wb") as f:
+          json.dump(res, f, indent=4)
 
